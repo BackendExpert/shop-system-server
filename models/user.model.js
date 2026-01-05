5123f97fb509f9ff53ed9d85893249ead4b3ffc3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-01-05 07:43:19 +0530

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -7,6 +7,7 @@ const UserSchema = new mongoose.Schema({
     role: { type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true },
     isActive: { type: Boolean, default: true },
     login_attempt: { type: Number, default: 0 },
+    lastLoginAttemptAt: { type: Date },
     lastLogin: Date,
 });
 
