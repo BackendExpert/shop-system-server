567415f924c30c3bd079ce32c5631dcbe0ca9a1a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-01-05 07:36:32 +0530

    Update

---
+++ models/user.model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    fullName: { type: String, trim: true },
+    username: { type: String, unique: true, lowercase: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    role: { type: mongoose.Schema.Types.ObjectId, ref: "Role", required: true },
+    isActive: { type: Boolean, default: true },
+    login_attempt: { type: Number, default: 0 },
+    lastLogin: Date,
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
