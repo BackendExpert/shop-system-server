c21832486eb6482f2bfdf0a2c4f2cdf254ea1b44
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-01-06 08:37:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ require("dotenv").config();
 // Example: const authRoute = require("./routes/authRoute");
 
 const authRoute = require("./routes/auth.route")
+const cetegoryRoute = require("./routes/category.route")
 
 // ---------------------- END of Importing Routes
 
@@ -106,7 +107,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // app.use('/api/auth', authRoute)
 
 app.use('/api/auth', authRoute)
-
+app.use('/api/cetegory', cetegoryRoute)
 
 // -------------- END routes-----------------------
 
